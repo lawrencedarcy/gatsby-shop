5dbeb540f074a64d4a00135f00e3eb19ac9e78e9
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-10-13 16:16:07 +0100

    add topping schema

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,32 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping name',
+      type: 'string',
+      description: 'Name of topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: { layout: 'checkbox' },
+      description: 'Is this topping suitable for vegetarians?',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🥬' : ''}`,
+    }),
+  },
+};
